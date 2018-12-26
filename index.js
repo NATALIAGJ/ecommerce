9df82cd1e9243f0366aee572c56c1b5c0162e5cb
Natalia Gonzalez Junco <gonzalezjunconatalia@gmail.com>
2018-12-26 14:31:11 -0500

    configuraciones de mongo en mlab

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const productsApiRouter = require('./routes/api/products');
 
+// App
 const app = express();
 
-app.use("/api/products", productsApiRouter);
-
+// Middlewares
 app.use(bodyParser.json());
 
+// static files
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api/products', productsApiRouter);
+
+// Redirect
+app.get('/', function(req, res) {
+  res.redirect('/api/products');
+});
+
+// Server
 const server = app.listen(8000, function() {
   console.log(`Listening http://localhost:${server.address().port}`);
 });
--- routes/api/products.js
+++ routes/api/products.js
@@ -17,4 +17,58 @@ router.get("/", async function(req, res, next) {
   }
 });
 
+router.get("/:productId", async function(req, res, next) {
+  const { productId } = req.params;
+  try {
+    const product = await productService.getProduct({ productId });
+    res.status(200).json({
+      data: product,
+      message: "product retrieved"
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async function(req, res, next) {
+  const { body: product } = req;
+  try {
+    const createProduct = await productService.createProduct({ product });
+    res.status(200).json({
+      data: createProduct,
+      message: "product create"
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:productId", async function(req, res, next) {
+  const { productId } = req.params;
+  const { body: product } = req;
+  try {
+    const updateProduct = await productService.updateProduct({ productId, product });
+    res.status(200).json({
+      data: updateProduct,
+      message: "product update"
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+router.delete("/:productId", async function(req, res, next) {
+  const { productId } = req.params;
+  try {
+    const deleteProduct = await productService.deleteProduct({ productId });
+    res.status(200).json({
+      data: deleteProduct,
+      message: "product delete"
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
